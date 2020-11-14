90b72e24e0930ce6e00520cbee4c17cb8e5cb0a9
root <root@prdx-ansible101.home.local>
2020-11-13 22:51:17 -0500

    first commit

---
+++ roles/mariadb/templates/init_team_members.sql
@@ -0,0 +1,7 @@
+create table team_members (member_id int NOT NULL, teamid int NOT NULL, member_name varchar(70) NOT NULL, member_state varchar(2) NOT NULL, primary key (member_id));
+
+insert into team_members values ('1','3','Kimberly Tyler','TX');
+insert into team_members values ('2','3','Angelica Ablang','TN');
+insert into team_members values ('3','3','Anna Primkulova','VA');
+insert into team_members values ('4','3','Anugraha Kondian Rajaram','VA');
+insert into team_members values ('5','3','Kathya Angel','VA');
